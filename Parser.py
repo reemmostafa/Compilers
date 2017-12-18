3a8d8f922b9056b72124e63fac865c1fb287113c
Ahmed Nasser <ahmed_nasser@outlook.com>
2017-12-18 03:36:54 +0200

    basic structure of parser class.

---
+++ Parser.py
@@ -0,0 +1,54 @@
+import Scanner
+
+
+class Parser:
+    def __init__(self):
+        pass
+
+    def match(self):
+        pass
+
+    def program(self):
+        pass
+
+    def stmt_sequence(self):
+        pass
+
+    def statement(self):
+        pass
+
+    def if_stmt(self):
+        pass
+
+    def repeat_stmt(self):
+        pass
+
+    def assign_stmt(self):
+        pass
+
+    def read_stmt(self):
+        pass
+
+    def write_stmt(self):
+        pass
+
+    def exp(self):
+        pass
+
+    def simple_exp(self):
+        pass
+
+    def comparison_op(self):
+        pass
+
+    def term(self):
+        pass
+
+    def add_op(self):
+        pass
+
+    def factor(self):
+        pass
+
+    def mul_op(self):
+        pass
